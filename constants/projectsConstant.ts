a784677cc8f21399c1a46da3698d341e70c86ee7
Izet Delibasic <izetdelibasic55@gmail.com>
2024-11-04 17:49:44 +0100

    Adding projects constant with projects data

---
+++ constants/projectsConstant.ts
@@ -0,0 +1,12 @@
+import { velezProject } from "./imagesConstant";
+
+export const projectsConstant = [
+  {
+    id: 1,
+    title: "RK Velež - Website",
+    description:
+      "first production project: a dynamic web application for the Velež handball club, built with NextJs and powered by Firebase and Firestore. It showcases the club's history, achievements, results, and news.",
+    image: velezProject,
+    href: "https://www.rk-velez.com/",
+  },
+];

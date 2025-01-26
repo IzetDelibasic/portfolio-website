2e51194e03f59ca6b242ff5150404ee4c3ab5676
Izet Delibasic <izetdelibasic55@gmail.com>
2025-01-26 23:59:36 +0100

    Updating education data

--- constants/educationConstant.ts
+++ constants/educationConstant.ts
@@ -1,6 +1,7 @@
-import { FaPushed } from "react-icons/fa";
 import { FaCodePullRequest } from "react-icons/fa6";
-import { GiStarShuriken } from "react-icons/gi";
+import { DiNodejsSmall } from "react-icons/di";
+import { DiJsBadge } from "react-icons/di";
+import { DiAngularSimple } from "react-icons/di";
 
 export const educationConstant = [
   {
@@ -13,13 +14,20 @@ export const educationConstant = [
     educationTitle: "JAVASCRIPT COURSE - CULTURAL CENTRE KING FAHD",
     educationDescription:
       " Intermediate level course, DOM Manipulation and API integration",
-    icon: FaPushed,
+    icon: DiJsBadge,
   },
   {
     educationTitle: "MERN 2024 EDITION - JOHN SMILGA",
     educationDescription:
       "Building full-stack application using MongoDB, Express, React and NodeJS - Udemy Course",
     href: "https://udemy-certificate.s3.amazonaws.com/image/UC-210c2883-855b-4bd7-a7f2-5f5ca51d31df.jpg?v=1713014388000",
-    icon: GiStarShuriken,
+    icon: DiNodejsSmall,
+  },
+  {
+    educationTitle: "ANGULAR and ASP.NET Core REST API",
+    educationDescription:
+      "Building full-stack application using Angular and ASP.NET Core REST API - Udemy Course",
+    href: "https://udemy-certificate.s3.amazonaws.com/image/UC-d6585be5-9004-4601-8e59-174fb3c4dd9e.jpg?v=1736716049000",
+    icon: DiAngularSimple,
   },
 ];
